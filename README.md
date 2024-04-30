bc0667505ccca26d8908c7c10d758b98d5fc2350
Pablo Giress <pablogiress@gmail.com>
2024-04-29 23:04:19 -0500

    Se sube readme y se realizan ajustes

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-<h3>CONVERTIDOR DE DIVISAS💵</h3>
+<h2>CONVERTIDOR DE DIVISAS💵</h2>
 
-<h5>Descripción</h5>
+<h3>Descripción</h3>
 <p>
     El siguiente proyecto es un convertidor de divisas realizado en JAVA aplicando
     la programación orientada a objetos.
     El usuario podra seleccionar diferentes divisas y el monto que desea convertir.
 </p>
 
+<h3>Guía de uso</h3>
+1. El usuario debe escribir el número de la divisa base que va convertir y presionar la tecla enter.
+2. El usuario debe escribir el número de la divisa objetivo y presionar la tecla enter.
+3. El usuario debe digitar el monto del valor a convertir y presionar enter.
+4. El sistema muestra al usuario el resultado de la conversión.
+
+Videotutorial: 
+
+<h3>API usada</h3>
+1. API Exchangerate - https://www.exchangerate-api.com/
 
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class Principal {
+import com.oraclealura.exchangerateapi.ConsultaDivisa;
+import com.oraclealura.exchangerateapi.Divisa;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        /*
+        * --------------------------------------------------------------------------------------
+        * Este bloque pregunta al usuario las divisas a convertir ------------------------------
+        * --------------------------------------------------------------------------------------*/
+        Scanner lectura = new Scanner(System.in);
+
+        boolean cicloDeVida = true;
+
+        while(cicloDeVida){
+
+        System.out.println("*******************CONVERTIDOR DE DIVISAS***********************");
+
+        //Preguntamos divisa base
+        System.out.println("Escriba el número de la divisa base a convertir:");
+        System.out.println("""
+                1. COP - Peso colombiano
+                2. USD - Dolar estadounidense
+                3. EUR - Euros
+                4. JPY - Yen japones
+                5. GBP - Libra esterlina
+                6. AUD - Dolar australiano
+                7. CAD - Dolar canadiense
+                8. CHF - Franco suizo
+                """);
+        var divisaBase =  lectura.nextInt(); //Guardamos divisa base
+
+
+        //Instanciamos convertidor de numero a codigo ISO
+        ConvierteDivisaIso convierteBase = new ConvierteDivisaIso();
+        String codigoIsoDivisaBase = convierteBase.convertirDivisaBase(divisaBase);
+
+
+        //Preguntamos divisa objetivo a convertir
+        System.out.println("Escriba el número de la divisa objetivo a convertir:");
+        System.out.println("""
+                1. COP - Peso colombiano
+                2. USD - Dolar estadounidense
+                3. EUR - Euros
+                4. JPY - Yen japones
+                5. GBP - Libra esterlina
+                6. AUD - Dolar australiano
+                7. CAD - Dolar canadiense
+                8. CHF - Franco suizo
+                """);
+        var divisaObjetivo =  lectura.nextInt(); //Guardamos divisa objetivo
+
+        //Instanciamos convertidor de numero a codigo ISO
+        ConvierteDivisaIso convierteObjetivo = new ConvierteDivisaIso();
+        String codigoIsoDivisaObjetivo = convierteObjetivo.convertirDivisaObjetivo(divisaObjetivo);
+
+        System.out.println("Digite el monto("+codigoIsoDivisaBase+") a convertir");
+        double monto = lectura.nextDouble();
+
+        /*-------------------------------------------------------------------------------------------
+         *--Este bloque de codigo inicializa la clase para consultar la conversion de dos divisas  --
+         * ----------------------------------------------------------------------------------------**/
+        if(monto > 0){
+            ConsultaDivisa consulta = new ConsultaDivisa();
+
+            String montoTexto = Double.toString(monto);
+            //Realizamos consulta a API
+            Divisa divisa = consulta.buscaDivisa(codigoIsoDivisaBase, codigoIsoDivisaObjetivo, montoTexto);
+            System.out.println("\n----------------------RESULTADO----------------------------");
+            System.out.println(monto+"("+codigoIsoDivisaBase + ") equivalen a "+divisa.conversion_result()+"("+codigoIsoDivisaObjetivo+")\n");
+            System.out.println("Presione 9 para continuar o presione 0 para salir");
+
+            int continuar = lectura.nextInt();
+
+            if(continuar == 0){
+                cicloDeVida = false;
+            }
+        }
+
+        }
+
+    }
 }
